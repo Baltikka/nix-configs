a02f01a21b5b6efae702d457e6d25dec2087c315
Ярослав Воробьев <73462095+Baltikka@users.noreply.github.com>
2026-08-28 15:18:18 +0700

    Create boot.nix

---
+++ modules/boot.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  # Автоматическая очистка старых снапшотов старше 7 дней
+  nix.gc = {
+    automatic = true;
+    dates = "02:00";           # Желаемое время
+    persistent = true;         # Эта опция включена по умолчанию
+    options = "--delete-older-than 7d";
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+}
