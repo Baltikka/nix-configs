12db13bcf57c9f316bb970e38bbc890c0b4035e4
Ярослав Воробьев <73462095+Baltikka@users.noreply.github.com>
2026-08-28 15:18:20 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,22 +8,8 @@
       ./modules/work.nix
       ./modules/network.nix
       ./modules/multimedia.nix
+      ./modules/boot.nix
     ];
-
-  # Автоматическая очистка старых снапшотов старше 7 дней
-  nix.gc = {
-    automatic = true;
-    dates = "02:00";           # Желаемое время
-    persistent = true;         # Эта опция включена по умолчанию
-    options = "--delete-older-than 7d";
-  };
-  
-  services.pulseaudio.support32Bit = true;
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -97,7 +83,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  programs.git.enable = true;
   environment.systemPackages = with pkgs; [
   fastfetch
   ];
